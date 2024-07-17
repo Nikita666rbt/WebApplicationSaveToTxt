97451735087896da4354c5ecf683c7a82179af9e
Никита Литовка <nikituslitus@yandex.ru>
2024-07-17 21:37:44 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Users users)
+        {
+            if (ModelState.IsValid) 
+            {
+                string filePath = Server.MapPath("~/App_Data/Пользователи.txt");
+                string fileData = $"{users.FullName};{users.Address};{users.Phone};{users.Email};";
+
+                using (StreamWriter sw = new StreamWriter(filePath, true))
+                {
+                    sw.WriteLine(fileData);
+
+                }
+                TempData["Message"] = "Данные успешно сохранены!";
+                return RedirectToAction("Index");
+            }
+            return View("Index", users);
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Users
+    {
+        [Required]
+        public string FullName {  get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone {get; set; }
+    }
+}
